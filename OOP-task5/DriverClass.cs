2b99162d54b42d658a663b5e11eca0ed4c05245c
FrankCiceri <frankanthonyc00@gmail.com>
2023-04-20 16:00:35 -0500

    Finished polynomial

---
+++ OOP-task5/DriverClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPtask5
+{
+    internal class DriverClass
+    {
+
+        public static void Main(string[] args) {
+            //I made the constructor so it receives the coefficients from the greater to the less exponent 
+            // Bassically I am doing  – 12x^7  + 5x^5 + 4x^2 - 3 
+            Poly poly1 = new Poly(-12, 0, 5, 0, 0, 0, 4,-3);
+            Console.WriteLine(poly1);
+
+
+            Poly poly2 = new Poly(-1, 0, 5, 0, 0, 1, 4, -3);
+            Console.WriteLine(poly2);
+
+            Console.WriteLine(poly1+poly2);
+
+            Console.WriteLine(poly1 - poly2);
+
+            Console.WriteLine(poly1 * poly2);  //https://shorturl.at/hAOQ0 url for the comparison of the result, you can use the same page for + and -
+
+        }
+
+    }
+}
---
+++ OOP-task5/Poly.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+
+namespace OOPtask5
+{
+    public class Poly
+    {
+        private readonly int[] coeffs;
+
+
+
+        public Poly(params int[] coeffs)
+        {
+
+            this.coeffs = coeffs;
+        }
+
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = coeffs.Length - 1; i >= 0; i--)
+            {
+                if (coeffs[i] == 0) // if the value we are working with is 0, it means that we have no coefficient there
+                    continue;
+
+                if (i != coeffs.Length - 1 && coeffs[i] > 0) //if the coeffieficcient is not the last one, and is positive
+                    sb.Append(" + ");                        // then we can append a +, that way we avoid placing a + when our first element
+                else if (coeffs[i] < 0)                      // in the sb is positive
+                    sb.Append(" - ");
+
+                int absVal = Math.Abs(coeffs[i]);
+              
+                if (absVal != 1)
+                    sb.Append(absVal);
+
+                int exp = (coeffs.Length - 1) - i;
+                if (exp > 0)
+                    sb.Append(exp==1? "x" : "x^" + (exp));
+            }
+
+            return sb.ToString();
+        }
+
+
+
+        public static Poly operator +(Poly p1, Poly p2)
+        {
+            
+            var (min, max) = MinMax(p1, p2);
+            int[] newCoeffs = new int[max];
+
+            for (int i = 0; i < min; i++) {
+                newCoeffs[i] = p1.coeffs[i] + p2.coeffs[i];
+            
+            }
+
+            for (int i = min; i < max; i++)
+            {
+                newCoeffs[i] = p1.coeffs.Length < p2.coeffs.Length ? p2.coeffs[i] : p1.coeffs[i];
+
+            }
+
+            return new Poly(newCoeffs);
+             
+        }
+        public static Poly operator -(Poly p1, Poly p2)
+        {
+
+            var (min, max) = MinMax(p1, p2);
+            int[] newCoeffs = new int[max];
+
+            for (int i = 0; i < min; i++)
+            {
+                newCoeffs[i] = p1.coeffs[i] - p2.coeffs[i];
+
+            }
+
+            for (int i = min; i < max; i++)
+            {
+                newCoeffs[i] = p1.coeffs.Length < p2.coeffs.Length ? -p2.coeffs[i] : p1.coeffs[i];
+
+            }
+
+            return new Poly(newCoeffs);
+
+        }
+
+        public static Poly operator *(Poly p1, Poly p2)
+        {
+
+            int[] newCoeffs = new int[p1.coeffs.Length + p2.coeffs.Length - 1];
+
+            for (int i = 0; i < p1.coeffs.Length; i++)
+            {
+                for (int j = 0; j < p2.coeffs.Length; j++)
+                {
+                    newCoeffs[i + j] += p1.coeffs[i] * p2.coeffs[j];
+                }
+            }
+
+            return new Poly(newCoeffs);
+
+        }
+
+        private static (int, int) MinMax(Poly p1, Poly p2) {
+         
+            if (p1.coeffs.Length <=p2.coeffs.Length)
+            {
+                return (p1.coeffs.Length, p2.coeffs.Length);
+            }
+            
+          return (p2.coeffs.Length, p1.coeffs.Length);
+
+        }
+    }
+}
